017e70b427a7d9c5014747ed93221c96103637d8
raresteak <raresteak@users.noreply.github.com>
2019-12-09 19:22:08 -0600

    Create post_install2.sh

---
+++ post_install2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt update
+
+apt full-upgrade
+
+apt install jq at idle3 vim vim-doc byobu python3-paramiko
+
+pip3 install pyperclip beautifulsoup4 selenium openpyxl PyPDF2 python-docx imapclient pyzmail twilio pillow python-xlib pyautogui
+
+echo "alias ll='ls -hl' >> ~/.bashrc
+
